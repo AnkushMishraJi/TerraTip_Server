ad71f08753c4d52f6d9941391399d6b1a636ac89
AnkushMishraJi <ankush.rdso@gmail.com>
2025-07-19 14:18:21 +0530

    added validation sample for user routes

--- routes/client/userRoutes.js
+++ routes/client/userRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
-
+const { addUser, generateToken } = require("../../validation/client/user");
+const validate = require('../../middlewares/validate');
 const userController = require('../../controllers/userController');
 const { verifyToken } = require('../../middlewares/auth');
-const { validateName, validateEmail, validatePhoneNumber, validatePropertyGeoLocation, validatePropertyAreaType, validatePropertySize } = require('../../validation/client/userValidation');
+const { validatePropertyGeoLocation, validatePropertyAreaType, validatePropertySize } = require('../../validation/client/userValidation');
 
-router.post('/', validateName, validateEmail, validatePhoneNumber, userController.newUserSignUp);
+router.post('/', validate(addUser), userController.newUserSignUp);
+router.post('/generateToken', validate(generateToken), userController.generateTokenFromUserDetails);
+
 router.post('/property', validatePropertyGeoLocation, validatePropertyAreaType, validatePropertySize, verifyToken, userController.addProperty);
-router.post('/generateToken', validatePhoneNumber, userController.generateTokenFromUserDetails);
 router.get('/portfolio', verifyToken, userController.getPortfolio);
 router.get('/', verifyToken, userController.getAllProperties);
 
